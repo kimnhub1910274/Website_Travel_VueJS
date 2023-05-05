4f34764aa541889a6d526b365decd8e04a902be1
nhub1910274 <nhub1910274@student.ctu.edu.vn>
2023-05-05 11:04:11 +0700

    Finish

--- Project/src/components/pages/Footer.vue
+++ Project/src/components/pages/Footer.vue
@@ -25,17 +25,25 @@ export default {
 <footer class="py-5 " >
       <div class="container"/>
         <button onclick="topFunction()" id="mybtn" class="gotop" title="Go to top">
-          <i class="fa-solid fa-arrow-up"></i>
+            Về
         </button>
         <div class="row">
           <div class="col">
               <h5 class="text-light "><b>Trang</b></h5>
-              <ul class="nav flex-column">
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Trang chủ</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Giới Thiệu</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Địa Điểm</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-light">Các Tỉnh/ Thành</a></li>
-              </ul>
+              <ul class="nav flex-column ">
+                    <router-link :to="{name:'home'}" class="nav-link p-0 text-light" >
+                    Trang Chủ
+                    </router-link>
+                    <router-link :to="{name:'introduce'}" class="nav-link p-0 text-light" >
+                        Giới Thiệu
+                    </router-link>
+                    <router-link :to="{name:'news'}" class="nav-link p-0 text-light" >
+                        Tin Tức
+                    </router-link>
+                    <router-link :to="{name:'town'}" class="nav-link p-0 text-light" >
+                        Các Tỉnh/ Thành
+                    </router-link>
+                </ul>
           </div>
       
           <div class="col">
@@ -53,27 +61,6 @@ export default {
               <p class="text-light">Số điện thoại: 090.789.6768 </p>
           </div>
       
-          <div class="col">
-              <h5 class="text-light"><b>Lời nhắn</b></h5>
-              <div class="">
-                <div class="input-group mb-2">
-                    <span class="input-group-text" id="basic-addon1">Họ và tên: </span>
-                    <input type="" class="form-control" placeholder="Nhập họ và tên" aria-label="name" aria-describedby="">
-                </div>
-                <div class="input-group mb-2">
-                    <span class="input-group-text" id="basic-addon1">Email: </span>
-                    <input type="" class="form-control" placeholder="Nhập email của bạn" aria-label="email" aria-describedby="">
-                </div>
-                <div class="input-group">
-                    <span class="input-group-text">Lời nhắn</span>
-                    <textarea class="form-control" aria-label="With textarea"></textarea>
-                </div>
-                <div class="button py-2">
-                    <button type="button" class="btn btn_main" >Gửi</button>
-                    
-                </div>
-              </div> 
-          </div>
         </div>
       
       
